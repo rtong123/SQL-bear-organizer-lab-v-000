ef6d42436d318746f14454cefb2e5f77e0d55409
Rebecca Tong <rtong@buffalo.edu>
2018-03-12 21:29:18 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Mr. Chocolate", 2, "M", "Black", "Calm", "True");
+
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Rowdy", 5, "M", "White", "Anxious", "False");
+
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Tabitha", 10, "F", "Red", "Active", "False");
+
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Sergeant Brown", 9, "F", "Orange", "Calm", "True");
+
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Melissa", 1, "M", "Brown", "Hyper", "False");
+
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Grinch", 22, "F", "Pink", "Cold", "True");
+
+INSERT into bears(name, age, gender, color, temperament, alive)
+VALUES("Wendy", 19, "M", "Green", "Active", "False");
+
+INSERT into bears(age, gender, color, temperament, alive)
+VALUES(13, "M", "Black", "Calm", "True");
